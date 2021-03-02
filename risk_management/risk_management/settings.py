590c57e67da9a9382ea8c0e45b87f3a2f99277ef
Dickson <chibuzordick94@gmail.com>
2021-03-02 23:51:38 +0100

    Database Schema

--- risk_management/risk_management/settings.py
+++ risk_management/risk_management/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django_extensions',
+    'django_extensions',
     'django.contrib.staticfiles',
     'zappa_django_utils',
 
